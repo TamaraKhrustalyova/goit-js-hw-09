f85c8b59f7b575b653de16eafd25c43fd001cd87
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-05-27 18:45:42 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    bodyColor: document.querySelector('body'),
+};
+let timerId = null;
 
+refs.startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    changeBackgroundColor()
+    }, 1000);
+    disableStartButton();
+})
+
+function changeBackgroundColor() {
+    refs.bodyColor.style.backgroundColor = getRandomHexColor();
+  }
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+refs.stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  makeStartButtonActive();  
+})
+
+function disableStartButton() {
+    refs.startBtn.setAttribute('disabled', '');
+}
+
+function makeStartButtonActive() {
+    refs.startBtn.removeAttribute('disabled');
+}
