4d967fa41111d2c51bc2d09aad8368da94dad11f
TamaraKhrustalyova <122490057+TamaraKhrustalyova@users.noreply.github.com>
2023-05-27 15:46:06 +0000

    Deploying to gh-pages from @ TamaraKhrustalyova/goit-js-hw-09@f85c8b59f7b575b653de16eafd25c43fd001cd87 🚀

---
+++ 01-color-switcher.bf649337.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),bodyColor:document.querySelector("body")};let e=null;t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{t.bodyColor.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.setAttribute("disabled","")})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.bf649337.js.map
